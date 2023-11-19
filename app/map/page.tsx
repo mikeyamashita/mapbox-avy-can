574f11d1d3630e63421e94ce63d0ab7f54f21ee9
Mike Yamashita <mrmyamashita@gmail.com>
2023-11-18 17:34:03 -0700

    api key in env variables

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,25 +1,22 @@
-'use client'
 import * as mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useEffect } from 'react';
+
+export default function Page() {
+
+    initMap();
+
+    return <div id="mapbox" className="mapbox" style={{ height: '100vh', width: '100vw' }}></div>
+}
 
 var map: any;
 
 function initMap() {
     map = new mapboxgl.Map({
-        accessToken: 'pk.eyJ1IjoiZHJpZWQ5NDEiLCJhIjoiY2xvbTExcTFnMHg2ZDJpczIxNmF4NjZmZyJ9.TMCo9cXWrMmOLPGHzre38g',
+        accessToken: process.env.NEXT_PUBLIC_MAPBOX_KEY,
         container: 'mapbox', // container ID
         style: 'mapbox://styles/mapbox/streets-v12', // style URL
         center: [-113.4937, 53.5461], // starting position [lng, lat]
         zoom: 8, // starting zoom
     });
-}
-
-export default function Page() {
-
-    useEffect(() => {
-        initMap()
-    });
-
-    return <div id="mapbox" className="mapbox" style={{ height: '100vh', width: '100vw' }}></div>
 }
