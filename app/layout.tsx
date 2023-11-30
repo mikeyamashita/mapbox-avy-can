4120c7d38cc994f635adf99e2d3f32c706be5e41
Mike Yamashita <mrmyamashita@gmail.com>
2023-11-30 07:05:57 -0700

    extend map into status bar on ios

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='light'>
+      <head>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+      </head>
       <body>
         <ReduxProvider>
           <UIProvider>
